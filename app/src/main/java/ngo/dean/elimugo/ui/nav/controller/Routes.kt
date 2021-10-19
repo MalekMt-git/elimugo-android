984259581476ec533b4c4215a3dcf133670596f0
MalekMt-git <arman.mtkh@gmail.com>
2021-10-19 21:26:22 +0200

    base class of MainScreen And Language is Added

--- app/src/main/java/ngo/dean/elimugo/ui/nav/controller/Routes.kt
+++ app/src/main/java/ngo/dean/elimugo/ui/nav/controller/Routes.kt
@@ -5,4 +5,6 @@ import android.annotation.SuppressLint
 sealed class Routes(val route: String) {
     @SuppressLint("CustomSplashScreen")
     object SplashScreen : Routes( "SplashScreen")
+    object LanguageScreen : Routes( "LanguageScreen")
+    object MainScreen : Routes( "MainScreen")
 }
--- app/src/main/java/ngo/dean/elimugo/ui/screen/SplashScreen.kt
+++ app/src/main/java/ngo/dean/elimugo/ui/screen/SplashScreen.kt
@@ -1,31 +1,39 @@
 package ngo.dean.elimugo.ui.screen
 
+import android.annotation.SuppressLint
 import android.content.Context
+import android.graphics.drawable.Animatable
 import android.view.WindowManager
 import android.window.SplashScreenView
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.res.painterResource
 import androidx.core.view.WindowCompat
 import androidx.navigation.NavController
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
 import ngo.dean.elimugo.R
+import ngo.dean.elimugo.ui.nav.controller.Routes
 import ngo.dean.elimugo.ui.theme.ElimugoTheme
 
 @Composable
 fun SplashScreen(navController: NavController, context: Context) {
 
     ElimugoTheme {
-        SplashScreenView()
+        SplashScreenView(navController,context)
     }
 }
 
+@SuppressLint("RememberReturnType")
 @Composable
-fun SplashScreenView(){
+fun SplashScreenView(navController: NavController, context: Context){
 
     Box(
         contentAlignment = Alignment.Center,
@@ -37,5 +45,13 @@ fun SplashScreenView(){
             contentDescription = "Logo",
             modifier = Modifier.scale(1.0f)
         )
+    }
+
+    val animatedFloat = remember { (0f) }
+
+    LaunchedEffect(animatedFloat) {
+        delay(1000)
+
+        navController.navigate(Routes.LanguageScreen.route)
     }
 }
