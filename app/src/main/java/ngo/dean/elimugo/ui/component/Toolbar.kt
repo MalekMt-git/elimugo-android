f7605f476745c1d9dd8655111e0b2e7df03f1c78
MalekMt-git <arman.mtkh@gmail.com>
2022-01-22 10:43:19 +0100

    done

--- app/src/main/java/ngo/dean/elimugo/ui/component/Toolbar.kt
+++ app/src/main/java/ngo/dean/elimugo/ui/component/Toolbar.kt
@@ -4,14 +4,11 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.AbsoluteAlignment
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
